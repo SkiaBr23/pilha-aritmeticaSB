e5599ac0cc801aba52703f3df42ebd338d62c66a
Lucas Mota <lmotaribeiro@gmail.com>
2017-05-14 09:38:09 -0300

    Infixed to postfixed notation.

--- calcula.c
+++ calcula.c
@@ -1,217 +1,298 @@
 /*		Máquina de Pilha Aritmética
-*	Integrantes: 	Arthur Jaber Costato (13/0039993)
-*			
-*			
-*			
+*	Integrantes: 	Lucas Mota Ribeiro (12/0016885)
 *
 *	Descrição:	Este programa receberá uma expressão aritmética com notação infixa via linha de comando
 *			(com ou sem parênteses) (quatro operações básicas).
 *			Então ele então a converterá para notação pós-fixa
 *			Por fim, ele resolverá a expressão por meio de aritmética extendida.
-*	Estruturas de Dados:	
-*	Macros:		
-*	Funções:	
-*	
+*	Estruturas de Dados:
+*	Macros:
+*	Funções:
+*
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#define MAX_LEN 255 //Maior tamanho possível de input
+#define MAX_STACK 255 // Maximo tamanho da pilha
 
-/* Macro para destuir a pilha */
-#define destruirPilha(var) \
- while(var != NULL){ \
-	pilha_t* aux = var->prox; \
-	free(var); \
-	var = aux; \
+
+/* Pilha*/
+typedef struct opStack{
+    char ops[MAX_STACK];
+    int top;
+}opStack;
+
+opStack * newStack()
+{
+    opStack *stack =(opStack*) malloc(sizeof(opStack));
+    stack->top = -1;
+    return stack;
 }
 
-/* Tipo de dado com 64 bits que representa no máximo 48 bits */
-typedef long long int int64;
-typedef struct pilha_t pilha_t;
-
-typedef struct uint48_t uint48_t;
-struct __attribute__((packed)) uint48_t {
-	int64 v:48;
-};
-
-/* Struct da pilha */
-struct pilha_t{
-	uint48_t num;	// Valor numérico
-	char op;	// Caractere da operação
-	pilha_t* prox;	// Próximo elemento da pilha
-};
-
-/* Função para criação da pilha inicializando com um número */
-void pushN(uint48_t num, pilha_t** prox){
-	pilha_t* pilha = (pilha_t*)malloc(sizeof(pilha_t));
-	if(pilha == NULL){
-		printf("Erro! Falta de memória!");
-		//destruirPilha(*prox)
-		// Como finalizar o programa?
-	}
-	pilha->num = num;
-	pilha->op = 0;
-	pilha->prox = *prox;
-	*prox = pilha;
+void push(opStack *stack, char op)
+{
+    if (stack->top != MAX_STACK) {
+        stack->ops[++stack->top] = op;
+    } else {
+        printf("Pilha ultrapassou tamanho máximo.\n");
+    }
 }
 
-/* Função para criação da pilha inicializando com uma operação */
-void pushO(char op, pilha_t** prox){
-	pilha_t* pilha = (pilha_t*)malloc(sizeof(pilha_t));
-	if(pilha == NULL){
-		printf("Erro! Falta de memória!");
-		//destruirPilha(*prox)
-		// Como finalizar o programa?
-	}
-	pilha->num.v =  0;
-	pilha->op = op;
-	pilha->prox = *prox;
-	*prox = pilha;
+char pop(opStack *stack)
+{
+    char op = 0;
+    if  (stack->top != -1) {
+        op = stack->ops[stack->top];
+        stack->ops[stack->top--] = 0;
+    }
+    return op;
+}
+char check(opStack *stack)
+{
+    char op = 0;
+    if  (stack->top != -1) {
+        op = stack->ops[stack->top];
+    }
+    return op;
 }
 
-/* Retira topo da pilha e retorna como número */
-uint48_t popN(pilha_t** pilha){
-	uint48_t num = (*pilha)->num;
-	pilha_t* prox = (*pilha)->prox;
-	free(*pilha);
-	(*pilha) = prox;
-	return num;
+int cmpPrecedence(char a, char b)
+{
+    enum { // Menor valor, maior precedência
+        OPEN = 2,
+        CLOSE = 2,
+        MULT = 5,
+        DIV = 5,
+        ADD = 6,
+        SUB = 6
+    };
+    int val_a, val_b;
+    switch (a) {
+        case '(':
+            val_a = OPEN;
+            break;
+        case ')':
+            val_a = CLOSE;
+            break;
+        case '*':
+            val_a = MULT;
+            break;
+        case '/':
+            val_a = DIV;
+            break;
+        case '+':
+            val_a = ADD;
+            break;
+        case '-':
+            val_a = SUB;
+            break;
+    }
+    switch (b) {
+        case '(':
+            val_b = OPEN;
+            break;
+        case ')':
+            val_b = CLOSE;
+            break;
+        case '*':
+            val_b = MULT;
+            break;
+        case '/':
+            val_b = DIV;
+            break;
+        case '+':
+            val_b = ADD;
+            break;
+        case '-':
+            val_b = SUB;
+            break;
+    }
+    return val_a - val_b;
 }
 
-/* Retira topo da pilha e retorna como operação */
-char popO(pilha_t** pilha){
-	char op = (*pilha)->op;
-	pilha_t* prox = (*pilha)->prox;
-	free(*pilha);
-	(*pilha) = prox;
-	return op;
+/* Converte a equação infixada para uma posfixada pelo algoritmo Shunting-yard.*/
+char* infixedToRPN(char *input)
+{
+    char *buffer = (char*) malloc(sizeof(char) * MAX_LEN * 2 - 1);
+    opStack *stack = newStack();
+    char* cont = input;
+    int offset = 0;
+    // printf("char: %d\n", *cont);
+    while(*cont != 0) { // Percorre a string
+        int readNumber = 0;
+        while(*cont >= '0' && *cont <= '9') { // Pega todos o digitos em sequencia
+            int toWrite = MAX_LEN * 2 - 1 - offset;
+            int written = snprintf(buffer + offset, toWrite, "%c", *cont);
+            if (toWrite < written) {
+                break;
+            }
+            offset += written;
+            readNumber = 1;
+            cont++;
+        }
+        snprintf(buffer + offset, MAX_LEN * 2 - 1 - offset, " ");
+        offset++;
+
+        switch (*cont) {
+            case '(':
+                push(stack, '(');
+            break;
+            case ')':
+                while (check(stack) != '(' && check(stack) != 0){
+                    int toWrite = MAX_LEN * 2 - 1 - offset;
+                    char ch = pop(stack);
+                    int written = snprintf(buffer + offset, toWrite, "%c ", ch);
+                    offset += written;
+                }
+                char final = pop(stack);
+                if(final != '('){
+                    printf("Casamento de parenteses falho\n");
+                }
+            break;
+            case '*':
+                while (check(stack) != 0 && cmpPrecedence('*', check(stack)) >= 0) {
+                    int toWrite = MAX_LEN * 2 - 1 - offset;
+                    int written = snprintf(buffer + offset, toWrite, "%c ", pop(stack));
+                    offset += written;
+                }
+                push(stack, '*');
+            break;
+            case '/':
+                while (check(stack) != 0 && cmpPrecedence('/', check(stack)) >= 0) {
+                    int toWrite = MAX_LEN * 2 - 1 - offset;
+                    int written = snprintf(buffer + offset, toWrite, "%c ", pop(stack));
+                    offset += written;
+                }
+                push(stack, '/');
+            break;
+            case '+':
+                while (check(stack) != 0 && cmpPrecedence('-', check(stack)) <= 0) {
+                    int toWrite = MAX_LEN * 2 - 1 - offset;
+                    int written = snprintf(buffer + offset, toWrite, "%c ", pop(stack));
+                    offset += written;
+                }
+                push(stack, '+');
+            break;
+            case '-':
+                while (check(stack) != 0 && cmpPrecedence('-', check(stack)) <= 0) {
+                    int toWrite = MAX_LEN * 2 - 1 - offset;
+                    int written = snprintf(buffer + offset, toWrite, "%c ", pop(stack));
+                    offset += written;
+                }
+                push(stack, '-');
+
+            break;
+        }
+        cont++;
+    }
+    while (check(stack) != 0) {
+        char ch = pop(stack);
+        int toWrite = MAX_LEN * 2 - 1 - offset;
+        // printf("BEFORE: str: %s\n", buffer);
+        int written = snprintf(buffer + offset, toWrite, "%c ", ch);
+        // printf("AFTER: str: %s\n", buffer);
+        // printf("str: %s| off: %d\n", buffer, offset);
+        offset += written;
+    }
+    // printf("str: %s| off: %d\n", buffer, offset);
+    return buffer;
 }
 
-/* Imprime a pilha */
-void imprimePilha(pilha_t* pilha){
-	while(pilha != NULL){
-			if(pilha->op == 0){
-				printf("%ld ", (long)pilha->num.v);
-			}else{
-				printf("%c ", pilha->op);
-			}
-			pilha = pilha->prox;
-	}
+/* Checa se a entrada é valida sintáticamente*/
+int checkIfValid(char *input)
+{
+    char *contador = input;
+    int ret = 1; //Retorno
+    while(*contador != 0) {
+        char ch = *contador++;
+        if (!(ch == '(' || ch == ')' || ch == '+' || ch == '-' || ch == '*' || ch == '/' || (ch >= '0' && ch <= '9'))){
+            // Encontrou caractere desconhecido.
+            ret = 0;
+            break;
+        }
+    }
+    return ret;
 }
 
-int main(int argc, char*argv[]){
-	if(argc == 1){
-		printf("Por favor, entre com a expressão na linha de comando!\n");
-		return 0;
-	}
-	
-	int i;
-	int negFlag = 0;
-	uint48_t num;
-	int parentesesAbertos = 0;
-	int operacao = 5; // 0 = recebeu um número, 1 = recebeu '+', 2 = recebeu '-', 3 = recebeu um '*', 4 = recebeu um '/', 5 = recebeu um '(' ou é início da expressão, 6 = recebeu um ')'
-	pilha_t* pilha = NULL;
-	for(i = 1; i < argc; i++){
-		if(*argv[i] == '('){
-			if(operacao == 0){ // Se recebeu um número, então multiplica
-				pushO('*', &pilha);
-			}
-			argv[i]++;	// Avança ponteiro de argv para o próximo caractere
-			parentesesAbertos++; // Incrementa a quantidade de parênteses abertos
-			pushO('(', &pilha);
-			operacao = 5;	// Sinaliza que última operação acrescenada na pilha foi um '('
-		}else if(*argv[i] == ')'){
-			argv[i]++;	// Avança ponteiro de argv para o próximo caractere
-			parentesesAbertos--; // Decrementa a quantidade de parênteses abertos
-			pushO(')', &pilha);
-			operacao = 6;	// Sinaliza que última operação acrescenada na pilha foi um ')'
-		}else if(*argv[i] == '+'){
-			argv[i]++;	// Avança ponteiro de argv para o próximo caractere
-			if(operacao != 1 && operacao != 2 && operacao != 5){ // Se já tiver recebido um '+' ou um '-' ou um '(' ou é início da expressão, não tem porquê acrescentar outro '+' na pilha
-				pushO('+', &pilha);
-				operacao = 1;// Sinaliza que última operação acrescenada na pilha foi um '+'
-			}
-		}else if(*argv[i] == '-'){
-			argv[i]++;	// Avança ponteiro de argv para o próximo caractere
-			if(operacao == 1){ // Se tiver recebido um '+', retira ele e coloca um '-'
-				popO(&pilha);
-				pushO('-', &pilha);
-				operacao = 2;// Sinaliza que última operação acrescenada na pilha foi um '-'
-			}
-			else if(operacao == 2){ // Se já tiver recebido um '-', retira ele e coloca um '+'
-				popO(&pilha);
-				if((pilha == NULL) || (pilha->op == '(')){
-					operacao = 5;// Não coloca +, só indica que operação anterior foi '(' ou início da expressão
-				}else{
-					pushO('+', &pilha);
-					operacao = 1;// Sinaliza que última operação acrescenada na pilha foi um '+'
-				}
-			}else{
-				pushO('-', &pilha);
-				operacao = 2;// Sinaliza que última operação acrescenada na pilha foi um '-'
-			}
-		}else if(*argv[i] == '*'){
-			argv[i]++;	// Avança ponteiro de argv para o próximo caractere
-			if(operacao == 1 || operacao == 2 || operacao == 4 || operacao == 5){ // Não pode colocar + * ou - * ou / * ou ( *
-				printf("Por favor, não coloque + * ou - * ou / * ou ( *\n");
-				destruirPilha(pilha)
-				return 0;
-			}else if(operacao != 3){ // Se já tiver recebido um '*', não tem porquê acrescentar outro '*' na pilha
-				// Acrescenta na pilha somente se tiver recebido um número ou ')'
-				pushO('*', &pilha);
-				operacao = 3;// Sinaliza que última operação acrescenada na pilha foi um '*'
-			}
-		}else if(*argv[i] == '/'){
-			argv[i]++;	// Avança ponteiro de argv para o próximo caractere
-			if(operacao == 1 || operacao == 2 || operacao == 3 || operacao == 4 || operacao == 5){ // Não pode colocar + / ou - / ou * / ou / / ou ( /
-				printf("Por favor, não coloque + / ou - / ou * / ou / / ou ( /\n");
-				destruirPilha(pilha)
-				return 0;
-			}else{
-				pushO('/', &pilha);
-				operacao = 4;
-			}
-		}else if((*argv[i]>='0' && *argv[i]<='9')){
-			if(operacao == 2){ // Se tiver um símbolo de negativo, retira ele e seta flag para alterar o sinal no final
-				popO(&pilha);
-				negFlag = 1;
-			}else if(operacao == 6){ // Se estiver fechando parênteses ')', então multiplica
-				pushO('*', &pilha);
-			}
-			
-			num.v = 0;
-			while((*argv[i]>='0' && *argv[i]<='9')){
-				num.v = 10*num.v + (*argv[i]-48);
-				argv[i]++;
-			}
-			if(*argv[i] == ',' || *argv[i] == '.'){
-				printf("Por favor, entre somente com números inteiros");
-				destruirPilha(pilha)
-				return 0;
-			}
-			if(negFlag){
-				num.v *= -1;
-				negFlag = 0;
-			}
-			pushN(num, &pilha);
-			operacao = 0;	// Sinaliza que foi acrescentado um número na pilha
-		}else{
-			printf("Por favor, entre somente com números inteiros ou operações: + - * / ( )");
-			destruirPilha(pilha)
-			return 0;
-		}
-		if(*argv[i] != '\0'){
-			i--;// Caso não tenha espaço, faz com que a próxima ineração volte para o mesmo argv
-		}
-	}
-	
-	if(parentesesAbertos){
-		printf("Por favor, verifique a quantidade de parênteses!");
-		destruirPilha(pilha)
-		return 0;
-	}
-	imprimePilha(pilha);
-	destruirPilha(pilha)
+/* Retira todos os espaços de uma string e retorna a quantidade de caracteres final.*/
+int removeEspacos(char* str)
+{
+    int size = 0;
+    char *caracter = str;
+    char *contador = str;
+    while(*contador != 0) {
+        *caracter = *contador++; // pega o próximo caracter.
+        size++;
+        if(*caracter != ' ') {
+            caracter++;
+        } else {
+            size--;
+        }
+    }
+    *caracter = 0; // Nulo no final
+    return size;
+}
+/* Método seguro de ler input sem dar overflow */
+int readInput(char *buffer, size_t bufsiz)
+{
+    char format[16];
+    snprintf(format, sizeof(format), "%%%zus", bufsiz - 1);
+    return scanf(format, buffer);
+}
+/* Pega o input de usuário pela entrada padrão */
+char* getUserInput(int *valid)
+{
+    char *buffer = (char*) malloc(sizeof(char) * MAX_LEN);
+    printf("Entre com a equação(%d caracteres no máximo)\n", MAX_LEN);
+    *valid = readInput(buffer, MAX_LEN);
+    return buffer;
+}
+
+/* Contatena o input por linha de comando e retorna a string e inpSize recebe o tamanho da entrada.*/
+char* concatInput(int ac, char **av, int *inpSize)
+{
+    char *buffer = (char*) malloc(sizeof(char) * MAX_LEN);
+    buffer[0] = 0;
+    int offset = 0;
+    *inpSize = 0;
+    while(av++, --ac) {
+       int toWrite = MAX_LEN - offset;
+       // Copia cada argv no buffer mais o offset do argv anterior.
+       int written = snprintf(buffer + offset, toWrite, "%s", *av);
+       if (toWrite < written) {
+           *inpSize += written;
+           break;
+       }
+       *inpSize += written;
+       offset += written;
+    }
+    return buffer;
+}
+
+int main(int argc, char*argv[])
+{
+    // Cecebe a equação.
+    int inputSize = 0;
+    char *input = concatInput(argc, argv, &inputSize);
+    if (inputSize > 0) {
+        printf("Recebeu equação por linha de comando: %s\n", input);
+    } else {
+        int validUserInp = 0; // 1 é OK, errado caso contrário.
+        while (!validUserInp) {
+            input = getUserInput(&validUserInp);
+            printf("%d - valid\n", validUserInp);
+            if (validUserInp && removeEspacos(input) == 0) {
+                printf("%d - valid2\n", validUserInp);
+                validUserInp = 0;
+            }
+        }
+    }
+    int valid = checkIfValid(input);
+    char *rpn = infixedToRPN(input);
+    printf("Posfixada: %s\n", rpn);
+
+    free(input); // Libera espaço de memória.
 	return 0;
 }
